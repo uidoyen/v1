c42c7e81be60163507c3ae4fba1c434c92b4677e
hussain <nasiba.ansari@gmail.com>
2020-07-27 22:45:13 +0530

    Removing loader

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,16 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
-import Prism from "prismjs"
 import styled from "styled-components"
 import { Helmet } from "react-helmet"
 import { GlobalStyle, Main } from "@styles"
 import Nav from "./nav"
 import Footer from "./footer"
-import Loader from "./loader"
 
 const StyledMain = styled(Main)``
 const Layout = ({ children, isFrontPage }) => {
-  const [isLoading, setIsLoading] = useState(isFrontPage);
   useEffect(() => {
     Prism.highlightAll()
   },[])
-  useEffect(() => {
-    if (isLoading || isFrontPage) {
-      return;
-    }
-  }, [isLoading])
   return (
     <>
       <Helmet>
@@ -26,16 +18,11 @@ const Layout = ({ children, isFrontPage }) => {
         <link href="https://use.typekit.net/fxm4txi.css" rel="stylesheet" />
       </Helmet>
       <GlobalStyle />
-      {isLoading && isFrontPage ? (
-        <Loader finishLoading={() => setIsLoading(false)} />
-        ) : (
         <div>
-          <Nav isFrontPage />
+          <Nav isFrontPage="isFrontPage" />
           <StyledMain>{children}</StyledMain>
           <Footer />
         </div>
-        )
-      }
     </>
   )
 }
